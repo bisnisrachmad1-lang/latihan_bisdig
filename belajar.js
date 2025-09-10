a9b77ad6bb7f94f32624b3666203ff5794382acb
rachmat <bisnisrachmad1@gmail.com>
2025-09-10 14:53:41 +0700

    latihan_bisdig

---
+++ belajar.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    alert('Selamat datang di website Rchmad Rmadhan!');
+    confirm("Apakah Anda ingin melanjutkan?");
+    prompt("Saya Belajar:");
+    
+    // Contoh penggunaan Date dan parseInt
+    const tanggal = new Date(10);
+    const angka = parseInt("123");
+    
+    // Menampilkan pesan setelah 2 detik
+    setTimeout(function() {
+        alert('Pesan 2 detik lagi');
+    }, 2000);
+    // Menampilkan pesan setelah 2 detik
+    setTimeout(function() {
+        alert('Pesan 3 detik lagi');
+    }, 2000);
+    // Menampilkan pesan setelah 2 detik
+    setTimeout(function() {
+        alert('Pesan 4 detik lagi');
+    }, 2000);
+    // Menampilkan pesan setelah 2 detik
+    setTimeout(function() {
+        alert('Pesan 5 detik lagi');
+    }, 2000);
+};
